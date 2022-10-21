5fc2315ae9d800bb2675fd0a320945a48e3074da
J-Skarbek <john@snarkmediaagency.com>
2022-10-21 16:56:31 -0500

    Test/add the date npm package.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import { formatDistance, subDays } from 'date-fns'
 import './style.css';
 import { createNav } from './nav-bar.js';
 import { createSideBar } from './sidebar.js';
@@ -10,6 +11,8 @@ newProject.logDeets();
 
 const task1 = ToDo('Make Dinner', '11/29/1986', 'High', 'Make it with low cholesterol');
 task1.logToDoDetails();
+
+console.log(formatDistance(subDays(new Date(), 3), new Date(), { addSuffix: true }))
 
 createNav();
 createSideBar();
