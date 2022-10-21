99ffcf5c2abca79b0040f8aeab39ba8c192412cb
J-Skarbek <john@snarkmediaagency.com>
2022-10-21 16:52:55 -0500

    Add to-do factory and protype.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import _ from 'lodash';
 import './style.css';
 import { createNav } from './nav-bar.js';
 import { createSideBar } from './sidebar.js';
-// import { ProjectPrototype } from './project-logic.js';
 import { Project } from './project-logic.js';
+import { ToDo } from './to-dos.js';
 
 const newProject = Project('New Project', 'John Skarbek')
 newProject.logDeets();
+
+const task1 = ToDo('Make Dinner', '11/29/1986', 'High', 'Make it with low cholesterol');
+task1.logToDoDetails();
 
 createNav();
 createSideBar();
---
+++ src/to-dos.js
@@ -0,0 +1,38 @@
+const ToDoProto = (name, date, priority, notes) => {
+  const logToDoDetails = () => {
+    console.log({name, date, priority, notes})
+  };
+
+  const updateStatus = () => {
+
+  };
+
+  const updateNotes = () => {
+
+  };
+
+  const markComplete = () => {
+
+  };
+
+  return {
+    updateStatus,
+    updateNotes,
+    markComplete,
+    logToDoDetails
+  }
+
+}
+
+export const ToDo = (name, date, priority, notes) => {
+  const {logToDoDetails} = ToDoProto(name, date, priority, notes);
+
+  return {
+    name,
+    date,
+    priority,
+    notes,
+    logToDoDetails
+  };
+
+}
